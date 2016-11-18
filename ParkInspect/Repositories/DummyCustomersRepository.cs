8329c20979859c42a5968abde284782000275dd2
Pim <pim.westervoort@gmail.com>
2016-11-18 17:12:18 +0100

    testdata added

--- ParkInspect/Repositories/DummyCustomersRepository.cs
+++ ParkInspect/Repositories/DummyCustomersRepository.cs
@@ -19,7 +19,11 @@ namespace ParkInspect.Repositories
         {
             return new List<CustomerViewModel>
             {
-                new CustomerViewModel(1, "Pim Westervoort", "5624KN", "06-tooawesomeforyou", 1, "pim.westervoort@gmail.com", "Noord-Brabant", "customer")
+                new CustomerViewModel(1, "Pim Westervoort", "5624KN", "06-tooawesomeforyou", 1, "pim.westervoort@gmail.com", "Noord-Brabant", "Klant"),
+                new CustomerViewModel(2, "Pim Westermoord", "4628JE", "06-tooawesomeforyou", 1, "pim.westermoort@gmail.com", "Noord-Holland", "Klant"),
+                new CustomerViewModel(3, "Pim Westerman", "8466UT", "06-tooawesomeforyou", 1, "pim.westerman@gmail.com", "Limburg", "Klant"),
+                new CustomerViewModel(4, "Pim Westerpoort", "4878HE", "06-tooawesomeforyou", 1, "pim.westerpoort@gmail.com", "Zuid-Holland", "Klant"),
+                new CustomerViewModel(5, "Pim Westernoord", "9922KK", "06-tooawesomeforyou", 1, "pim.westernoord@gmail.com", "Utrecht", "Klant")
             };
         }
 
