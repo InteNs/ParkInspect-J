8b82d4a6f7622e2b9834fe89474af636d1105ce1
Pim <Pim@LAPTOP_PIM>
2016-11-18 14:25:10 +0100

    startup failure fixed

--- ParkInspect/ViewModel/ViewModelLocator.cs
+++ ParkInspect/ViewModel/ViewModelLocator.cs
@@ -24,12 +24,14 @@ namespace ParkInspect.ViewModel
             //repositories
             SimpleIoc.Default.Register<ITestItemRepository, DummyTestItemRepository>();
             SimpleIoc.Default.Register<IEmployeeRepository, DummyEmployeesRepository>();
+            //SimpleIoc.Default.Register<ICustomerRepository, DummyEmployeesRepository>();
 
             //viewmodels
             SimpleIoc.Default.Register<MainViewModel>();
             SimpleIoc.Default.Register<TestViewModel1>();
             SimpleIoc.Default.Register<TestViewModel2>();
             SimpleIoc.Default.Register<EmployeesViewModel>();
+            SimpleIoc.Default.Register<CustomersViewModel>();
         }
         //router
         public RouterViewModel Router => ServiceLocator.Current.GetInstance<RouterViewModel>();
@@ -37,7 +39,8 @@ namespace ParkInspect.ViewModel
         public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
         public TestViewModel1 Test1 => ServiceLocator.Current.GetInstance<TestViewModel1>();
         public TestViewModel2 Test2 => ServiceLocator.Current.GetInstance<TestViewModel2>();
-   	    public EmployeesViewModel Employees => ServiceLocator.Current.GetInstance<EmployeesViewModel>(); 
+   	    public EmployeesViewModel Employees => ServiceLocator.Current.GetInstance<EmployeesViewModel>();
+        public CustomersViewModel Customers => ServiceLocator.Current.GetInstance<CustomersViewModel>();
 
 
         public EditEmployeeViewModel EditEmployee => new EditEmployeeViewModel(ServiceLocator.Current.GetInstance<IEmployeeRepository>(), Router, Employees);
