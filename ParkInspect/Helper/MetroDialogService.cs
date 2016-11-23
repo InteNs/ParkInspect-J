8401bedcd204f0f41399bcd56d95364dbc7e0e54
mfhbree <mfhbree@avans.nl>
2016-11-23 22:48:16 +0100

    Feature/styling (#58)
    * MetroStyle added
    
    * Styling done and window/previous completed
    
    * Styling finished and som fixes
    
    * Requested change fixed
    
    * Fixes
    
    * Fixes2
    
    * Fixes3
    
    * fix bindings

---
+++ ParkInspect/Helper/MetroDialogService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ParkInspect.Helper
+{
+    public class MetroDialogService
+    {
+        public  bool IsAffirmative { get; private set; }
+
+        private async Task<MessageDialogResult> ShowMessageAsync(string title, string message, MessageDialogStyle style = MessageDialogStyle.AffirmativeAndNegative, MetroDialogSettings settings = null)
+        {
+            return await ((MetroWindow)(Application.Current.MainWindow)).ShowMessageAsync(title, message, style, settings);
+        }
+
+        public async Task Show(string title, string message)
+        {
+            var res =
+                await
+                    ShowMessageAsync(title,
+                        message);
+            IsAffirmative = res == MessageDialogResult.Affirmative;
+        }
+    }
+}
