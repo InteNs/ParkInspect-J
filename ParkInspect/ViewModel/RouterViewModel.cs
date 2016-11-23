a65d054e58e1a5ef8b0c4e84e837184e07d55151
mfhbree <mfhbree@avans.nl>
2016-11-23 22:48:16 +0100

    Feature/styling (#58)
    * MetroStyle added
    
    * Styling done and window/previous completed
    
    * Styling finished and som fixes
    
    * Requested change fixed
    
    * Fixes
    
    * Fixes2
    
    * Fixes3
    
    * fix bindings

--- ParkInspect/ViewModel/RouterViewModel.cs
+++ ParkInspect/ViewModel/RouterViewModel.cs
@@ -10,15 +10,21 @@ namespace ParkInspect.ViewModel
 {
     public class RouterViewModel : ViewModelBase
     {
+
+        private Stack<UserControl> _previousViews;
+
         public UserControl CurrentView { get; set; }
 
         public ICommand SetViewCommand { get; set; }
+
+        public ICommand SetPreviousViewCommand { get; set; }
 
         private IDictionary<string, Type> _views;
 
         public RouterViewModel()
         {
             SetViewCommand = new RelayCommand<string>(SetView);
+            SetPreviousViewCommand = new RelayCommand(SetPreviousView);
             _views = new Dictionary<string, Type>
             {
                 { "employees-list", typeof(EmployeesView) },
@@ -32,14 +38,21 @@ namespace ParkInspect.ViewModel
                 { "Customers-add", typeof(AddCustomerView) },
                 { "dashboard-manager", typeof(DashboardManagerView) }
             };
-
             //Test
             SetView("dashboard-manager");
-
+            _previousViews = new Stack<UserControl>();
         }
         public void SetView(string viewName)
         {
+            _previousViews?.Push(CurrentView);
             this.CurrentView = (UserControl)Activator.CreateInstance(_views[viewName]);
+            RaisePropertyChanged("CurrentView");
+        }
+
+        private void SetPreviousView()
+        {
+            if (!(_previousViews?.Count > 0)) return;
+            this.CurrentView = _previousViews.Pop();
             RaisePropertyChanged("CurrentView");
         }
     }
