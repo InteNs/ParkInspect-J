5edd3c4f3490610397daaa546faef37f74408ee1
Edward van Lieshout <ehliesho@avans.nl>
2016-11-23 13:22:21 +0100

    i've got changes

---
+++ ParkInspect/ViewModel/CommissionOverviewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using ParkInspect.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ParkInspect.ViewModel
+{
+    class CommissionOverviewViewModel : ViewModelBase
+    {
+        private ICommand BackCommand;
+        private ICommand AddCommissionCommand;
+        private ICommissionRepository _icr;
+        private List<CommissionViewModel> _commissionList;
+        private List<EmployeeViewModel> _showableEmployeeList;
+        private List<EmployeeViewModel> _completeEmployeeList;
+        private CommissionViewModel _selectedCommission;
+
+        public CommissionOverviewViewModel(ICommissionRepository _icr)
+        {
+            this._icr = _icr;
+        }
+    }
+}
