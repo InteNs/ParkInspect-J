91a5abdbd627722798d5aec74c22bf109b295e28
kevinhagenaars <h.agenaars@hotmail.com>
2016-11-28 08:48:19 +0100

    Feature/inloggen logica (#55)
    * Logica
    
    * Bindings
    
    * Fixed Bindings
    
    * - Removed non-dummy repo
    - Passwords are now stored SHA256 hashed
    
    * Feature dashboard manager (#57)
    
    * Dashboard manager view
    
    Dashboard manager view + terug knop
    
    * Back button
    
    redirects back to dashboard-manager
    
    * Main window
    
    Main window goes to Dashboard management for testing purposes
    
    * Router met dashboard-manager
    
    * Feature/styling (#58)
    
    * MetroStyle added
    
    * Styling done and window/previous completed
    
    * Styling finished and som fixes
    
    * Requested change fixed
    
    * Fixes
    
    * Fixes2
    
    * Fixes3
    
    * fix bindings
    
    * Feature/opdrachtenoverzicht view (#59)
    
    * fundering
    
    * add opdrachten gemaakt
    
    * iets gedaan
    
    * Add opdrachten view verbonden met router
    
    * Dinges ofzo
    
    * binding add
    
    * add opdrachten af
    
    * i've got changes
    
    * Problemen na merge opgelost en CommissionOverviewViewModel afgemaakt tot zover mogelijk.
    
    * commissionoverview beetje
    
    * combobox en labels toegevoegd
    
    * Bindings voor de views van opdrachten afgemaakt
    
    * minor fixes
    
    * - changed backbuttin to white (#60)
    
    * hotfix
    
    * changed router yet again
    
    only issue right now is initial user control on launch
    
    * Foundation created for inspections (#74)
    
    * Finished Management (#75)
    
    * Finished Management
    
    Closes #23 #19
    
    * Fixed visibilities
    
    PLS
    
    * removed bullshit
    
    * add AuthService Interface

--- ParkInspect/ViewModel/ViewModelLocator.cs
+++ ParkInspect/ViewModel/ViewModelLocator.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 using ParkInspect.Repositories;
+using ParkInspect.Service;
 using ParkInspect.View;
 
 namespace ParkInspect.ViewModel
@@ -16,8 +17,10 @@ namespace ParkInspect.ViewModel
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-            //router
-            SimpleIoc.Default.Register<RouterViewModel>();
+            //services
+            SimpleIoc.Default.Register<IRouterService, RouterService>();
+            SimpleIoc.Default.Register<IAuthService, AuthService>();
+            //authe
             //database context
             SimpleIoc.Default.Register<ParkInspectEntities>();
             //repositories
@@ -35,6 +38,8 @@ namespace ParkInspect.ViewModel
             SimpleIoc.Default.Register<AddCommissionViewModel>();
             SimpleIoc.Default.Register<CommissionOverviewViewModel>();
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<AddCommissionViewModel>();
+            SimpleIoc.Default.Register<CommissionOverviewViewModel>();
             SimpleIoc.Default.Register<EmployeesViewModel>();
             SimpleIoc.Default.Register<ManagementRapportenViewModel>();
             SimpleIoc.Default.Register<AuthenticationViewModel>();
@@ -42,23 +47,23 @@ namespace ParkInspect.ViewModel
             SimpleIoc.Default.Register<QuestionsViewModel>();
             SimpleIoc.Default.Register<TemplatesViewModel>();
             SimpleIoc.Default.Register<InspectionsViewModel>();
-            
         }
-        //router
-        public RouterViewModel Router => ServiceLocator.Current.GetInstance<RouterViewModel>();
+        //Services
+        public IRouterService RouterService => ServiceLocator.Current.GetInstance<IRouterService>();
+        public IAuthService AuthService => ServiceLocator.Current.GetInstance<IAuthService>();
         //viewmodels
         public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
         public ManagementRapportenViewModel Management => ServiceLocator.Current.GetInstance<ManagementRapportenViewModel>();
    	    public EmployeesViewModel Employees => ServiceLocator.Current.GetInstance<EmployeesViewModel>();
         public QuestionsViewModel Questions => ServiceLocator.Current.GetInstance<QuestionsViewModel>();
         public TemplatesViewModel Templates => ServiceLocator.Current.GetInstance<TemplatesViewModel>();
-        public EditEmployeeViewModel EditEmployee => new EditEmployeeViewModel(ServiceLocator.Current.GetInstance<IEmployeeRepository>(), Router, Employees);
-        public AddEmployeeViewModel AddEmployee => new AddEmployeeViewModel(ServiceLocator.Current.GetInstance<IEmployeeRepository>(), Router, Employees);
+        public EditEmployeeViewModel EditEmployee => new EditEmployeeViewModel(ServiceLocator.Current.GetInstance<IEmployeeRepository>(), RouterService, Employees);
+        public AddEmployeeViewModel AddEmployee => new AddEmployeeViewModel(ServiceLocator.Current.GetInstance<IEmployeeRepository>(), RouterService, Employees);
         public AuthenticationViewModel Authentication => ServiceLocator.Current.GetInstance<AuthenticationViewModel>();
         public CustomersViewModel Customers => ServiceLocator.Current.GetInstance<CustomersViewModel>();
-        public AddCustomerViewModel AddCustomer => new AddCustomerViewModel(ServiceLocator.Current.GetInstance<ICustomerRepository>(), Router, Customers);
+        public AddCustomerViewModel AddCustomer => new AddCustomerViewModel(ServiceLocator.Current.GetInstance<ICustomerRepository>(), RouterService, Customers);
         public CommissionOverviewViewModel Commissions => ServiceLocator.Current.GetInstance<CommissionOverviewViewModel>();
-        public AddCommissionViewModel AddCommission => new AddCommissionViewModel(ServiceLocator.Current.GetInstance<ICommissionRepository>(), Router, Commissions);
+        public AddCommissionViewModel AddCommission => new AddCommissionViewModel(ServiceLocator.Current.GetInstance<ICommissionRepository>(), RouterService, Commissions);
         public InspectionsViewModel Inspections => ServiceLocator.Current.GetInstance<InspectionsViewModel>();
         public static void Cleanup()
         {
