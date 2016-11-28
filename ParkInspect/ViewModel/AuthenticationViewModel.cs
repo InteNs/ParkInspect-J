3200ba5616f79f8e99afa5e40031a513db6080a1
kevinhagenaars <h.agenaars@hotmail.com>
2016-11-28 13:59:48 +0100

    Moved to service

--- ParkInspect/ViewModel/AuthenticationViewModel.cs
+++ ParkInspect/ViewModel/AuthenticationViewModel.cs
@@ -17,32 +17,5 @@ namespace ParkInspect.ViewModel
         public  string Username { get; set; }
         public  int UserId { get; set; }
         public int EmployeeId { get; set; }
-
-        private IAuthenticationRepository _repo;
-        public ICommand DoLoginCommand { get; set; }
-
-        public AuthenticationViewModel(IAuthenticationRepository repo, IRouterService router) : base(router)
-        {
-            _repo = repo;
-            DoLoginCommand = new RelayCommand<PasswordBox>(DoLogin);
-            repo.FillUserFile();
-        }
-
-        private void DoLogin(PasswordBox password)
-        {
-            string[] user = _repo.Login(Username, password.Password);
-            if (user != null)
-            {
-                UserId = Convert.ToInt32(user[2]);
-                EmployeeId = Convert.ToInt32(user[3]);
-                Username = user[0];
-                RouterService.SetView("dashboard-manager");
-                
-                return;
-            }
-            MessageBox.Show("Foute inloggegevens!");
-
-
-        }
     }
 }
