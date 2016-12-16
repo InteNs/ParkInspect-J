ee40089fce094c9704f13730eae8cf62fdf38e39
Jeroen Vijgen <BlackChaosNL@users.noreply.github.com>
2016-12-16 14:51:50 +0200

    generieke tabellen (#96)
    * Finished Management
    
    Closes #23 #19
    
    * Fixed visibilities
    
    PLS
    
    * Added initial sql files
    
    * Table renamed and deleted GUID for QuestionItem
    
    * Base tables working and deployable
    
    Changed TIMESTAMP => TIME, and made sure the base tables are deployable.
    Will be doing foreign keys next.
    
    * Added a few foreign keys
    
    * Added some relationships
    
    Foreign keys for Commission, Customer, Employee and Person added.
    
    * Added final constraints
    
    Last FK constraints have been added, database is deployable (Tested on
    VS2015 + MSSQL V'12)
    
    * Changed location FKs in foreign tables
    
    Location now uses a Primary Key of ID + Guid for referencing.

---
+++ Database/Location.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Location]
+(
+	[Id] INT NOT NULL,
+	[StreetNumber] INT NOT NULL,
+	[ZipCode] varchar(6) NOT NULL,
+	[Guid] uniqueidentifier NOT NULL,
+	[RegionId] int NOT NULL,
+	[RegionGuid] uniqueidentifier NOT NULL
+
+	CONSTRAINT [PK_Location] PRIMARY KEY CLUSTERED
+	(
+		Id,
+		Guid
+	)
+
+	CONSTRAINT [FK_Location_Region] FOREIGN KEY (RegionId, RegionGuid) REFERENCES Region (Id, Guid)
+)
