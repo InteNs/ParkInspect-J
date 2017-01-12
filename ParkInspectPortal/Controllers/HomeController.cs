65ea593c60ea30f77b8f6051d1dcc1d113b62a14
kevinhagenaars <h.agenaars@hotmail.com>
2017-01-12 14:57:53 +0100

    Added PDF builder Added Asp site + list with inspections to download

---
+++ ParkInspectPortal/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ParkInspectPortal.Entities;
+using ParkInspectPortal.Helpers;
+using ParkInspectPortal.Models;
+
+
+namespace ParkInspectPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // TODO: get all inspections from DB
+            List<InspectionViewModel> Inspections = new List<InspectionViewModel>();
+            using (var ctx = new ParkInspectEntities())
+            {
+                var inspections = ctx.Inspection.ToList();
+                foreach(var item in inspections)
+                    Inspections.Add(new InspectionViewModel()
+                    {
+                        DateTimeEnd = item.DateTimeEnd,
+                        DateTimeStart = item.DateTimeStart,
+                        Guid = item.Guid,
+                        Id= item.Id,
+                    });
+            }
+
+                return View(Inspections);
+        }
+
+        public ActionResult Download(Guid Guid)
+        {
+            // TODO: Map data with AutoMapper from repo or DB
+            var pdfvm = new InspectionPDFViewModel();
+
+            using (var pdfBuilder = new PDFBuilder())
+            {
+                pdfBuilder.AddHtml(pdfvm, pdfBuilder.MapPath("/Views/_templates/PDF/Inspectie.cshtml"));
+                return File(pdfBuilder.GetPdfStream(), "application/pdf");
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
