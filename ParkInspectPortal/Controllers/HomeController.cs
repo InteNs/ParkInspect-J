f650b2eff25b6e39763a4d74ab1726e3164eeef3
kevinhagenaars <h.agenaars@hotmail.com>
2017-01-15 11:21:40 +0100

    Reference

--- ParkInspectPortal/Controllers/HomeController.cs
+++ ParkInspectPortal/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ParkInspectPortal.Entities;
+using Data;
+
 using ParkInspectPortal.Helpers;
 using ParkInspectPortal.Models;
 
