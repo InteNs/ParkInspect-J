6319c7e82af5c40df5f90dfc73b3ad50a328f0c3
Nurleyanti <nsakidin@avans.nl>
2017-01-16 16:16:31 +0100

    location ren region entitities af. niet getest

---
+++ ParkInspect/Repository/Entity/EntityRegionRepository.cs
@@ -0,0 +1,45 @@
+﻿using ParkInspect.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Data;
+
+namespace ParkInspect.Repository.Entity
+{
+    public class EntityRegionRepository : IRegionRepository
+    {
+        private readonly ParkInspectEntities _context;
+        private readonly ObservableCollection<string> _regions;
+
+        public EntityRegionRepository(ParkInspectEntities context)
+        {
+            _context = context;
+            _regions = new ObservableCollection<string>();
+        }
+
+        public bool Add(string item)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public bool Delete(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<string> GetAll()
+        {
+            _context.Region.ToList().ForEach(r => _regions.Add(r.RegionName));
+            return _regions;
+        }
+
+        public bool Update(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
