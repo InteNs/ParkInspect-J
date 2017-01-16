c211918de68f021c11867b6c9ae37f9714d6612d
Edward van Lieshout <ehliesho@avans.nl>
2017-01-16 16:31:48 +0100

    UNIT TESTS

--- ParkInspect/Converter/DatetimeToTimestringConverter.cs
+++ ParkInspect/Converter/DatetimeToTimestringConverter.cs
@@ -8,7 +8,7 @@ using System.Windows.Data;
 
 namespace ParkInspect.Converter
 {
-    class DatetimeToTimestringConverter : IValueConverter
+    public class DatetimeToTimestringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
