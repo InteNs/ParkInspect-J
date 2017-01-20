c91508e10e223fbe10e88849c8b79ee607e5d8dd
ehliesho <ehliesho@avans.nl>
2017-01-20 15:31:37 +0100

    magic

--- ParkInspect/ViewModel/InspectionsViewModel.cs
+++ ParkInspect/ViewModel/InspectionsViewModel.cs
@@ -7,18 +7,85 @@ namespace ParkInspect.ViewModel
     public class InspectionsViewModel : MainViewModel
     {
         private CommissionViewModel _selectedCommission;
+        private InspectionViewModel _selectedInspection;
         private bool _isManager;
         private bool _isInspecteur;
+        private IAuthService authservice;
+        private ObservableCollection<CommissionViewModel> _commissionList;
+        private ObservableCollection<InspectionViewModel> _inspectionList;
 
-        public ObservableCollection<InspectionViewModel> InspectionList { get; private set; }
-        public ObservableCollection<CommissionViewModel> CommissionList { get; private set; }
+        public ObservableCollection<InspectionViewModel> InspectionList
+        {
+            get
+            {
+                ObservableCollection<InspectionViewModel> insp = new ObservableCollection<InspectionViewModel>();
+                if (SelectedCommission == null)
+                {
+                    return insp;
+                }
+                foreach (InspectionViewModel ivm in _inspectionList)
+                {
+                    if(ivm.CommissionViewModel.Id == SelectedCommission.Id)
+                    {
+                        insp.Add(ivm);
+                    }
+                }
+                return insp;
+            }
+            private set
+            {
+                _inspectionList = value;
+                RaisePropertyChanged();
+            }
+        }
+        public ObservableCollection<CommissionViewModel> CommissionList
+        {
+            get
+            {
+                if(authservice.CurrentFunction(authservice.GetLoggedInUser()).ToLower() == "inspecteur")
+                {
+                    ObservableCollection<CommissionViewModel> coms = new ObservableCollection<CommissionViewModel>();
+                    foreach(CommissionViewModel cvm in _commissionList)
+                    {
+                        if(cvm.Employee.Id == authservice.GetLoggedInUser().EmployeeId)
+                        {
+                            coms.Add(cvm);
+                        }
+                    }
+                    return coms;
+                }
+                return _commissionList;
+            }
+            private set
+            {
+                _commissionList = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public CommissionViewModel SelectedCommission
         {
             get { return _selectedCommission; }
-            set { _selectedCommission = value; RaisePropertyChanged(); }
+            set { _selectedCommission = value; RaisePropertyChanged(); RaisePropertyChanged("InspectionList"); SelectedInspection = null; }
+        }
+
+        public InspectionViewModel SelectedInspection
+        {
+            get { return _selectedInspection; }
+            set { _selectedInspection = value; RaisePropertyChanged(); RaisePropertyChanged("EnableButtons"); }
         }
         
+        public bool EnableButtons
+        {
+            get
+            {
+                if(_selectedInspection == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
 
         public bool IsManager
         {
@@ -33,8 +100,9 @@ namespace ParkInspect.ViewModel
         }
 
         public InspectionsViewModel(ICommissionRepository commissionRepository,
-            IInspectionsRepository inspectionsRepository, IRouterService router) : base(router)
+            IInspectionsRepository inspectionsRepository, IRouterService router, IAuthService auth) : base(router)
         {
+            authservice = auth;
             switch (RouterService.CurrentDashboard)
             {
                 case "dashboard-inspecteur":
