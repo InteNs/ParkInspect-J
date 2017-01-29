3e4b32235f3ad5f80f2dd564aeb1ac43789bdead
Pim <pim.westervoort@gmail.com>
2017-01-29 19:39:48 +0100

    questiontests start

---
+++ ParkInspect.Tests/QuestionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkInspect.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkInspect.Tests
+{
+    [TestClass]
+    public class QuestionTests
+    {
+        [TestMethod]
+        [TestCategory("viewmodels")]
+        public void QuestionCreate()
+        {
+            QuestionViewModel question = new QuestionViewModel();
+            question.Id = 123;
+            question.Version = 234;
+            question.Description = "testcase";
+            question.IsActive = true;
+            question.QuestionType = Enumeration.QuestionType.Boolean;
+
+            Assert.AreEqual(question.Create(), question);
+            question.IsActive = true;
+            Assert.AreEqual(question.Create().IsActive, false);
+        }
+    }
+}
