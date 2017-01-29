891936bb9e36b0899cd9695a0017c98441adb5e6
Nurleyanti <nsakidin@avans.nl>
2017-01-29 16:48:48 +0100

    iets

---
+++ ParkInspect.Tests/InspectionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkInspect.Tests
+{
+    [TestClass]
+    public class InspectionTests
+    {
+        [TestMethod]
+        public void DoInspectionTest()
+        {
+
+        }
+
+    }
+}
